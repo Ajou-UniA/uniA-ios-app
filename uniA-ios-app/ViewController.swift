fed5793dfecf7e47ff6755a4797475cae8c4f6d2
Sarang Kim <vvtkfkddl11@naver.com>
2023-03-10 19:47:05 +0900

    Initial Commit

---
+++ uniA-ios-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uniA-ios-app
+//
+//  Created by 김사랑 on 2023/03/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
