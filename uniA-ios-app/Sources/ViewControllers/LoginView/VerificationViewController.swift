bb5bf0a325a80def4550c5d940618afc3f9aceb0
rudolfoss <sorktjsdldu1@naver.com>
2023-03-15 12:57:02 +0900

    [FEAT] added OTP code

--- uniA-ios-app/Sources/ViewControllers/LoginView/VerificationViewController.swift
+++ uniA-ios-app/Sources/ViewControllers/LoginView/VerificationViewController.swift
@@ -9,8 +9,9 @@ import SnapKit
 import Then
 import UIKit
 import SwiftUI
+import CHIOTPField
 
-class VerificationViewController: UIViewController {
+class VerificationViewController: UIViewController, UITextFieldDelegate {
     //MARK: - Properties
 
     lazy var titleLabel = UILabel().then {
@@ -25,20 +26,42 @@ class VerificationViewController: UIViewController {
         $0.font = UIFont.systemFont(ofSize: 13)
         $0.numberOfLines = 2
     }
+    lazy var otpField = CHIOTPFieldOne().then{
+        $0.numberOfDigits = 4
+        $0.borderColor = .lightGray
+        $0.cornerRadius = 8
+        $0.spacing = 18
+        $0.font = UIFont.systemFont(ofSize: 20)
+        $0.keyboardType = .numberPad
+    }
+    
+    lazy var submitBtn = UIButton().then {
+        $0.setTitle("Submit", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = .systemBlue
+        $0.layer.cornerRadius = 10
+    }
+    lazy var resendBtn = UIButton().then {
+        $0.setTitle("Resend Code", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 13, weight: .semibold)
+
+    }
     
     //MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        
+        otpField.delegate = self
+
         setUpView()
         setUpConstraints()
     }
     //MARK: - Helper
 
     func setUpView() {
-        [titleLabel,subtitleLabel].forEach {
+        [titleLabel,subtitleLabel,otpField,submitBtn,resendBtn].forEach {
             view.addSubview($0)
         }
     }
@@ -56,7 +79,26 @@ class VerificationViewController: UIViewController {
             $0.width.equalTo(Constant.width * 340)
             $0.height.equalTo(Constant.height * 40)
         }
+        otpField.snp.makeConstraints {
+            $0.top.equalTo(subtitleLabel.snp.bottom).offset(25)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(Constant.width * 314)
+            $0.height.equalTo(Constant.height * 50)
+        }
+        submitBtn.snp.makeConstraints {
+            $0.top.equalTo(otpField.snp.bottom).offset(34)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(Constant.width * 315)
+            $0.height.equalTo(Constant.height * 52)
+        }
+        resendBtn.snp.makeConstraints {
+            $0.top.equalTo(submitBtn.snp.bottom).offset(12)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(Constant.width * 85)
+            $0.height.equalTo(Constant.height * 22)
+        }
     }
+   
 }
 //MARK: - SwiftUI
 
