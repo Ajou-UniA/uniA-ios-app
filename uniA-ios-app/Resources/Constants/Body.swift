58470c68232cc9ff97ef587840659eb6724a5ceb
vvtkfkddl11 <vvtkfkddl11@naver.com>
2023-05-06 21:25:27 +0900

    [FEAT] added Const

---
+++ uniA-ios-app/Resources/Constants/Body.swift
@@ -0,0 +1,16 @@
+//
+//  Body.swift
+//  uniA-ios-app
+//
+//  Created by 김사랑 on 2023/05/06.
+//
+
+import Foundation
+
+extension Const {
+    struct Body {
+        static let paramBody = ["Content-Type": "multipart/form-data"]
+        static var param = ["loginId": "userID@ajou.ac.kr", "password": "password"]
+    }
+}
+
---
+++ uniA-ios-app/Resources/Constants/UserDefaultsKey.swift
@@ -0,0 +1,19 @@
+//
+//  UserDefaultsKey.swift
+//  uniA-ios-app
+//
+//  Created by 김사랑 on 2023/05/03.
+//
+
+import Foundation
+
+extension Const {
+    struct UserDefaultsKey {
+        static let socialType = "socialType"
+        static let accessToken = "accessToken"
+        static let sessionId = "sessionId"
+        static let memberId = "memberId"
+        static let updatedAt = "updatedAt"
+        static let isLogin = "isLogin"
+    }
+}
