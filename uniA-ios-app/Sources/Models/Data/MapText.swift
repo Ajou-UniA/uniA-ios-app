6bfbae7e2e8a49c69e7d4c6ee14a22c66bdb5d71
rudolfoss <sorktjsdldu1@naver.com>
2023-06-01 00:52:06 +0900

    [FEAT] added TextCollectionView

---
+++ uniA-ios-app/Sources/Models/Data/MapText.swift
@@ -0,0 +1,29 @@
+//
+//  MapText.swift
+//  uniA-ios-app
+//
+//  Created by HA on 2023/05/31.
+//
+
+import UIKit
+
+var mapText = [ccc, fff, xxx,jjj, mmm, rrr]
+
+var aaa = "1 Main Gate\n\n2 School Bus Stop\n\n3 The Pioneer Statue\n\n4 Energy Center\n\n5 Woncheon Hall\n\n6 Woncheon Haedong Hall\n\n"
+var bbb = "7 North Gate\n\n8 Chemical Engineering Laboratory\n\n9 West Hall\n\n10 East Hall"
+var ccc = aaa + bbb
+var ddd =  "11 Capstone Design Laboratory\n\n12 Geotechnical Engineering Laboratory\n\n13 Civil Engineering Laboratory\n\n14 Paldal Hall\n\n15 R.O.T.C Headquarters\n\n"
+var eee = "16 Industry-University Cooperation Building\n\n17 International House (Dormitory 5)\n\n18 Gwanggyo Hall (Dormitory 4)\n\n19 Hwahong Hall(Dormitory 3)\n\n20 Yongji Hall (Dormitory2)"
+var fff = ddd + eee
+var ggg = "21 Dormitory  Dining Hall\n\n22 Namje Hall (Dormitory 1)\n\n23 Student Union 1\n\n24 Student Union 2\n\n"
+var qqq = "25 Seongho Hall\n\n26 The Terrace\n\n27 Central Library\n\n28 Amphitheater\n\n29 Yulgok Hall\n\n30 Dasan Hall"
+var xxx = ggg + qqq
+var hhh = "31 College of Pharmacy\n\n32 Clinical Skills Laboratory and Animal Research Center\n\n33 Yeonam Hall\n\n34 Tennis Courts\n\n35 School of Medicine-Research Building\n\n"
+var iii = "36 Parking Garage 3\n\n37 Gymnasium\n\n38 Athlectic field\n\n39 Campus Plaza\n\n40 Sonjae Hall"
+var jjj = hhh + iii
+var kkk = "41 Ajou University Hospital\n\n42 University Hospital Annex\n\n43 Well-Being Center\n\n44 Parking Garage 2\n\n45 Funeral Hall\n\n46 South Gate\n\n"
+var lll = "47 University Hospital Main Gate\n\n48 Trauma Center\n\n49 East Gate\n\n50 Honhjae Hall"
+var mmm = kkk + lll
+var nnn = "51 Gaon Garden\n\n52 Ilsin Hall\n\n53 Hyegang Hall\n\n54 Connecting Square\n\n55 Mirae Hall\n\n"
+var ooo = "56 Emergency Medical Center\n\n57 Ajou University Intermediate Care Hospital\n\n58 Parking Garage 1"
+var rrr = nnn + ooo
---
+++ uniA-ios-app/Sources/ViewControllers/HomeView/TextCollectionViewCell.swift
@@ -0,0 +1,55 @@
+//
+//  TextCollectionViewCell.swift
+//  uniA-ios-app
+//
+//  Created by HA on 2023/05/31.
+//
+
+import SnapKit
+import Then
+import UIKit
+
+class TextCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier: String = "TextCollectionViewCell"
+    
+    let textView = UITextView().then {
+        $0.isEditable = false
+        $0.font = UIFont(name: "Urbanist-SemiBold", size: 14)
+        $0.showsVerticalScrollIndicator = false
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+        setUpConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpView() {
+        self.contentView.addSubview(textView)
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineHeightMultiple = 1.2  // Adjust the line spacing as desired
+            
+            let attributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont(name: "Urbanist-SemiBold", size: 14)!,
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            textView.typingAttributes = attributes
+        
+    }
+    
+    func setUpConstraint() {
+        textView.snp.makeConstraints {
+            $0.top.bottom.leading.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(15)
+        }
+        
+    }
+    
+}
