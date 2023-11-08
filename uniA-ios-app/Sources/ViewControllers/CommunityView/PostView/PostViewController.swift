1380799f6b7867bba4c03feadd4dbf83951a6c78
vvtkfkddl11 <vvtkfkddl11@naver.com>
2023-11-08 16:35:57 +0900

    [FEAT] PostView 기초 작업

---
+++ uniA-ios-app/Sources/ViewControllers/CommunityView/PostView/PostViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PostViewController.swift
+//  uniA-ios-app
+//
+//  Created by 김사랑 on 2023/11/08.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+}
